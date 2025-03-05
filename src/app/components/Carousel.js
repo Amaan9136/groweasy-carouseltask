34d7f28fe85e5500b09e54d7a82ce553edc4bd6b
Amaan MK <syedkhalander66@gmail.com>
2025-03-05 15:26:59 +0530

    feat: gsap to slide between images

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -21,27 +21,11 @@ export default function Carousel() {
   const imageContainerRef = useRef(null);
 
   const nextSlide = () => {
-    gsap.to(imageContainerRef.current, {
-      x: "-100%",
-      duration: 0.8,
-      ease: "power2.inOut",
-      onComplete: () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        gsap.set(imageContainerRef.current, { x: "0%" });
-      },
-    });
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const prevSlide = () => {
-    gsap.to(imageContainerRef.current, {
-      x: "100%",
-      duration: 0.8,
-      ease: "power2.inOut",
-      onComplete: () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-        gsap.set(imageContainerRef.current, { x: "0%" });
-      },
-    });
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   const startAutoPlay = () => {
@@ -49,9 +33,7 @@ export default function Carousel() {
   };
 
   const stopAutoPlay = () => {
-    if (intervalRef.current) {
-      clearInterval(intervalRef.current);
-    }
+    clearInterval(intervalRef.current);
   };
 
   useEffect(() => {
@@ -63,28 +45,35 @@ export default function Carousel() {
     return () => stopAutoPlay();
   }, [isPlaying]);
 
+  useEffect(() => {
+    gsap.fromTo(
+      imageContainerRef.current,
+      { opacity: 0, x: 100 },
+      { opacity: 1, x: 0, duration: 1, ease: "power2.out" }
+    );
+  }, [currentIndex]);
+
   return (
     <div className="relative flex flex-col items-center justify-center p-3 bg-white text-black w-full h-screen">
       <div className="relative flex items-center justify-center w-full max-w-6xl h-[80vh] overflow-hidden">
-        <div ref={imageContainerRef} className="flex w-full justify-center items-center">
-          <img
-            src={images[(currentIndex - 1 + images.length) % images.length]}
-            alt="prev"
-            className="w-1/3 h-[75vh] object-cover rounded-lg scale-90 opacity-80"
-          />
-
-          <img
-            src={images[currentIndex]}
-            alt="current"
-            className="w-1/2 h-[80vh] object-cover rounded-lg scale-100 shadow-lg"
-          />
-
-          <img
-            src={images[(currentIndex + 1) % images.length]}
-            alt="next"
-            className="w-1/3 h-[75vh] object-cover rounded-lg scale-90 opacity-80"
-          />
-        </div>
+        <img
+          src={images[(currentIndex - 1 + images.length) % images.length]}
+          alt="prev"
+          className="w-1/3 h-[75vh] object-cover rounded-lg scale-90 opacity-80 transition-transform duration-1000 ease-in-out transform translate-x-[-10%]"
+        />
+        
+        <img
+          ref={imageContainerRef}
+          src={images[currentIndex]}
+          alt="current"
+          className="w-1/2 h-[80vh] object-cover rounded-lg scale-100 shadow-lg"
+        />
+        
+        <img
+          src={images[(currentIndex + 1) % images.length]}
+          alt="next"
+          className="w-1/3 h-[75vh] object-cover rounded-lg scale-90 opacity-80 transition-transform duration-1000 ease-in-out transform translate-x-[10%]"
+        />
       </div>
 
       <div className="flex gap-2 mt-6">
@@ -103,14 +92,12 @@ export default function Carousel() {
         <button onClick={prevSlide} className="p-4 bg-black rounded-full text-white hover:bg-gray-800">
           <IoIosArrowBack size={28} />
         </button>
-
         <button
           onClick={() => setIsPlaying(!isPlaying)}
           className="p-4 bg-black rounded-full text-white hover:bg-gray-800"
         >
           {isPlaying ? <FaPause size={24} /> : <FaPlay size={24} />}
         </button>
-
         <button onClick={nextSlide} className="p-4 bg-black rounded-full text-white hover:bg-gray-800">
           <IoIosArrowForward size={28} />
         </button>
