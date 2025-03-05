f37055fc68be3d49b1a196f472ca1ad17df74c47
Amaan MK <syedkhalander66@gmail.com>
2025-03-05 15:23:44 +0530

    chore: Task Completion

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -1,5 +1,4 @@
 "use client";
-
 import gsap from "gsap";
 import { useEffect, useRef, useState } from "react";
 import { FaPause, FaPlay } from "react-icons/fa";
@@ -19,38 +18,40 @@ export default function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(true);
   const intervalRef = useRef(null);
-  const carouselRef = useRef(null);
+  const imageContainerRef = useRef(null);
 
   const nextSlide = () => {
-    gsap.to(carouselRef.current, {
+    gsap.to(imageContainerRef.current, {
       x: "-100%",
-      duration: 1,
-      ease: "power3.inOut",
+      duration: 0.8,
+      ease: "power2.inOut",
       onComplete: () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        gsap.set(carouselRef.current, { x: "0%" });
+        gsap.set(imageContainerRef.current, { x: "0%" });
       },
     });
   };
 
   const prevSlide = () => {
-    gsap.to(carouselRef.current, {
+    gsap.to(imageContainerRef.current, {
       x: "100%",
-      duration: 1,
-      ease: "power3.inOut",
+      duration: 0.8,
+      ease: "power2.inOut",
       onComplete: () => {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-        gsap.set(carouselRef.current, { x: "0%" });
+        gsap.set(imageContainerRef.current, { x: "0%" });
       },
     });
   };
 
   const startAutoPlay = () => {
-    intervalRef.current = setInterval(nextSlide, 4000);
+    intervalRef.current = setInterval(nextSlide, 3000);
   };
 
   const stopAutoPlay = () => {
-    clearInterval(intervalRef.current);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
   };
 
   useEffect(() => {
@@ -65,12 +66,23 @@ export default function Carousel() {
   return (
     <div className="relative flex flex-col items-center justify-center p-3 bg-white text-black w-full h-screen">
       <div className="relative flex items-center justify-center w-full max-w-6xl h-[80vh] overflow-hidden">
-        <div ref={carouselRef} className="flex w-full h-full translate-x fade-in-out transition-transform duration-1000 ease-in-out">
+        <div ref={imageContainerRef} className="flex w-full justify-center items-center">
+          <img
+            src={images[(currentIndex - 1 + images.length) % images.length]}
+            alt="prev"
+            className="w-1/3 h-[75vh] object-cover rounded-lg scale-90 opacity-80"
+          />
+
           <img
             src={images[currentIndex]}
             alt="current"
-            className="w-full h-[80vh] object-cover rounded-lg shadow-lg"
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
+            className="w-1/2 h-[80vh] object-cover rounded-lg scale-100 shadow-lg"
+          />
+
+          <img
+            src={images[(currentIndex + 1) % images.length]}
+            alt="next"
+            className="w-1/3 h-[75vh] object-cover rounded-lg scale-90 opacity-80"
           />
         </div>
       </div>
@@ -91,12 +103,14 @@ export default function Carousel() {
         <button onClick={prevSlide} className="p-4 bg-black rounded-full text-white hover:bg-gray-800">
           <IoIosArrowBack size={28} />
         </button>
+
         <button
           onClick={() => setIsPlaying(!isPlaying)}
           className="p-4 bg-black rounded-full text-white hover:bg-gray-800"
         >
           {isPlaying ? <FaPause size={24} /> : <FaPlay size={24} />}
         </button>
+
         <button onClick={nextSlide} className="p-4 bg-black rounded-full text-white hover:bg-gray-800">
           <IoIosArrowForward size={28} />
         </button>
